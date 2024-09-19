1b2234f721449ed7a9bf6d802637f0995a009611
Ranito <45947472+Ranito@users.noreply.github.com>
2024-09-19 11:42:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from logger import setup_logging
 
 def main():
     
-    parser = argparse.ArgumentParser(description="Synchronize two folders.")
+    parser = argparse.ArgumentParser(description="Sync two folders.")
     parser.add_argument("source", help="Path to the source folder")
     parser.add_argument("replica", help="Path to the replica folder")
-    parser.add_argument("interval", type=int, help="Synchronization interval in seconds")
+    parser.add_argument("interval", type=int, help="Sync interval in seconds")
     parser.add_argument("log_file", help="Path to the log file")
 
     sync_file = parser.parse_args()
